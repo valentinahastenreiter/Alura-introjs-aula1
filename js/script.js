90db744e6b6051746b6b286750a1661b5f37459f
valentina.hastenreiter <valentina.moraes@pmviana.local>
2022-03-09 17:41:23 -0300

    acréscimo de verificações de altura e peso

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+for(var i=0; i < pacientes.length;  i++){
+    
+var paciente = pacientes[i];
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+
+if(peso <= 0 || peso >= 200){
+    console.log("Peso inválido!");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido!";
+    paciente.classList.add("paciente-invalido")
+
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!")
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida"
+    paciente.classList.add("paciente-invalido")
+}
+
+if (alturaValida && pesoValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+}
+}
+
+
+
